148700ca05cc02cc9babe1a5e22b31d1125e13b8
Luca Gagliardi <lucaj.1991@gmail.com>
2023-09-27 16:17:12 +0200

    added procedures typically used now for learning tentacle mac speed motion

--- main.py
+++ main.py
@@ -3,27 +3,30 @@ from learning import actionValue
 import numpy as np
 
 
-#TO DO: Equilibrate a bit the system before training..
+#TODO: Equilibrate a bit the system before training.. <--
 
 carrierMode = 1
 n_suckers = 8
-sim_shape = (30,)
-t_position = 29.5
+sim_shape = (60,)
+t_position = 59 
 
 
+#EPISODIC LEARNING
 
 episodes =1000
-steps = 500
+steps = 5000
 
 env = Environment(n_suckers,sim_shape,t_position,carrierMode=carrierMode)
 Q =actionValue((env.state_space,env.action_space),nAgents=env._nagents,n_episodes=episodes)
 #OBS: action array is identically formulated for both multiagent and single. What changes is the indexing strategy
-tstepsTotarget =[]
-episode = []
-diff =[] #to check convergence
-Q_old = Q._Q.copy()
+
 for e in range(episodes):
     state = env.get_state()
+    if (e%20==0):
+        print(e)
+        print("convergence =", Q.get_conv())
+        print("lr =",Q.lr)
+        print("epsilon =", Q.epsilon)
     for k in range(steps):
         action = Q.get_action(state)
         old_state = state
@@ -32,34 +35,31 @@ for e in range(episodes):
         # if (e%500==0):
         #     env.render()
         if terminal:
-            # print('touched wall')
-            print(Q.get_value())
             break
-    if (e%200==0):
-        print(e)
-        print(Q.get_value())
-    diff.append(np.amax(np.abs(Q._Q -Q_old)))
-    Q_old = Q._Q.copy()
     Q.makeGreedy()
-    tstepsTotarget.append(k)
-    episode.append(env._episode)
-    env.reset()
-# Q.get_policyView()
-import matplotlib.pyplot as plt
-plt.figure()
-fig = plt.subplot(xlabel='episodes', ylabel='time_steps to target')
-fig.plot(epoch,tstepsTotarget)
-plt.ion()
-plt.show()
-    # print("episode",env._epoch)
-    # Q.makeGreedy() #decrease epsilon
+    env.reset(exploringStarts=True)
 
+#############
+#CONTINUOUS LEARNING
 
-    # rivedi dinamica.. dovrebbe essere instantanea l'evoluzione
-    #   a questo punto però c'è una scelta da fare su quando viene evolto ogni agente..
-    # update action ogni mezzo periodo? (ma Q ad ogni time step?)
-    # reward prop a velocita centro di massa
-
-    #quando pensi funziona, compara con random policy
-
-    #wish list: read papear, learn something, achieve a code that does something, include one goal with a collaboration
+episodes =1000
+steps = 2000
+sim_shape = (40,)
+t_position = 41 #target out of simulation box 
+env = Environment(n_suckers,sim_shape,t_position,carrierMode=carrierMode)
+Q =actionValue((env.state_space,env.action_space),nAgents=env._nagents,n_episodes=episodes)
+#OBS: action array is identically formulated for both multiagent and single. What changes is the indexing strategy
+state = env.get_state()
+for e in range(episodes):
+    if (e%20==0):
+        print(e)
+        print("convergence =", Q.get_conv())
+        print("lr =",Q.lr)
+        print("epsilon =", Q.epsilon)
+    for k in range(steps):
+        action = Q.get_action(state)
+        old_state = state
+        state,reward,terminal  = env.step(action)
+        Q.update(state,old_state,action,reward)
+    Q.makeGreedy() #just for scheduling, not linked to an actual episode
+    env.reset_partial() #to avoid abuse of memory DOES NOT reset position 
