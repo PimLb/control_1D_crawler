93dbb40bbde4314c9575ec5cefff00a657d867ff
Luca Gagliardi <lucaj.1991@gmail.com>
2023-10-09 18:16:34 +0200

    Q matrix is now a dictionary.. Working in progress to exploit the new data structure.

--- learning.py
+++ learning.py
@@ -1,15 +1,21 @@
 #TODO implement tools to monitor learning (value computation, convergence ecc)
+# Make Q matrix a dictionary for efficiency <---
+# MAKE DIFFERENT Q MATRIX FOR TIP AND BASE: with dictionary is straight forward but plotting methods change ok
+# Give states as tuple in dictionary
+# Initialize Q according to dedicated funtion generating all available dictionary keys (like 'base', intermediate i, tip ecc)
 
 from globals import *
 import numpy as np
+import copy 
 
 max_lr = 0.1# was 0.3 learning rate
-min_lr = 0.0025 # was 0.05 then 0.01
+min_lr = 0.0025 # was 0.05 then 0.01, for hive 0.0025
 gamma = 0.9#discount 0.9 to reflect upon..
 max_epsilon = 0.9
-min_epsilon =0.01
+min_epsilon =0.0025
 
-
+stateName =['->|<- ','->|-> ','->|tip ','<-|<- ','<-|-> ','<-|tip ','base|<- ','base|-> ']
+actionState=[' not anchoring',' anchoring']
 
 def interpret_binary(s:tuple):
     return int("".join(str(ele) for ele in s), 2)
@@ -51,10 +57,15 @@ class actionValue(object):
                 print("\n** HIVE UPDATE **\n")
                 self.update = self._update_Q_parallel
                 self.get_action = self._get_action_hiveUpdate
-                Q = np.random.random(self.dim)
+                Q={}
+                #k could be whatsover even directly the tuple of states
+                #   --> Can drop the interpreter but need a function producing all possible states
+                for k in range(self.dim[0]):
+                    Q[k] = np.random.random(self.dim[1])
+                # Q = np.random.random(self.dim)
                 self._Q = Q
                 #convergence observables:
-                self._oldQ = self._Q.copy()  
+                self._oldQ = copy.deepcopy(self._Q)
                 self._convergence = []
                 #VALUE
                 # second index to contain correspondent policy. For 2 action space, 0 or 1 (= argmax) to be associated to color in plot
@@ -77,13 +88,35 @@ class actionValue(object):
                 self._Q = []
                 print("\n** NOT hive update **\n")
                 print("A Q matrix per agent")
+                print("(however epsilon, lr and gamma are universal)")
                 self._value =[]
                 self._av_value = []
                 self._oldQ =[]
-                for k in range(nAgents):
-                    Q = np.random.random(self.dim)#each agent has Q initialized differently
+                self._convergence = []
+                
+                #BASE
+                Q={}
+                Q[6] = np.random.random(self.dim[1])
+                Q[7] = np.random.random(self.dim[1])
+                
+                self._Q.append(Q) 
+                self._oldQ.append(Q)
+                Q={}
+                #INTERMEDIATE ONES
+                Q[0] = np.random.random(self.dim[1])
+                Q[1] = np.random.random(self.dim[1])
+                Q[3] = np.random.random(self.dim[1])
+                Q[4] = np.random.random(self.dim[1])
+                for k in range(1,self._nAgents-1):
                     self._Q.append(Q) 
                     self._oldQ.append(Q)
+                # Q = np.zeros(self.dim)
+                #TIP
+                Q={}
+                Q[2] = np.random.random(self.dim[1])
+                Q[5] = np.random.random(self.dim[1])
+                self._Q.append(Q) 
+                self._oldQ.append(Q)
                 self.get_value = self._get_value_noHive
                 self.get_av_value = self._get_av_value_noHive
                 self._get_diff = self._get_diff_noHive
@@ -101,33 +134,26 @@ class actionValue(object):
             self._multiAgent = False
             self.get_action = self._get_action_singleAgent 
             self.dim = learning_space #+ (nAgents,) not sure
+    
         
-
-
-        
-        self._fig_value = None
-        self._fig_av_value =None
-        self._fig_convergence = None
-
-        
-    def reset(self):
-        self.n_episodes = 0
-        self._Q = np.random.random(self.dim)
-        self.epsilon = max_epsilon
-        self.lr = max_lr
+    # def reset(self):
+    #     self.n_episodes = 0
+    #     self._Q = np.random.random(self.dim)
+    #     self.epsilon = max_epsilon
+    #     self.lr = max_lr
     
     def _get_index(self,state,action=0):
-        if self._multiAgent:
+        # if self._multiAgent:
             #here action is a scalar and state a 2 elements list. 
             # I need to consider dummy action argument, when I need only state
             #i = interpret_binary(state)
-            i = interpret_thernary(state)
-            j = action
-        else:
-            #to check..
-            # i = interpret_binary(state)
-            i = interpret_thernary(state)
-            j = interpret_binary(action)
+        i = interpret_thernary(state)
+        j = action
+        # else:
+        #     #to check..
+        #     # i = interpret_binary(state)
+        #     i = interpret_thernary(state)
+        #     j = interpret_binary(action)
             
         return i,j
     def _update_Q_parallel(self,newstate,oldstate,action,reward):
@@ -140,7 +166,7 @@ class actionValue(object):
         for  k in range(0,self._nAgents):
             s_new,_a_new = self._get_index(newstate[k]) 
             s_old,a_old = self._get_index(oldstate[k],action[k])
-            self._Q[s_old,a_old] += self.lr* (reward + gamma * np.amax(self._Q[s_new]) - self._Q[s_old,a_old])
+            self._Q[s_old][a_old] += self.lr* (reward + gamma * np.amax(self._Q[s_new]) - self._Q[s_old][a_old])
 
         
 
@@ -149,7 +175,7 @@ class actionValue(object):
         for  k in range(self._nAgents):
             s_new,_a_new = self._get_index(newstate[k]) 
             s_old,a_old = self._get_index(oldstate[k],action[k])
-            self._Q[k][s_old,a_old] += self.lr* (reward + gamma * np.amax(self._Q[k][s_new]) - self._Q[k][s_old,a_old])
+            self._Q[k][s_old][a_old] += self.lr* (reward + gamma * np.amax(self._Q[k][s_new]) - self._Q[k][s_old][a_old])
 
             #This could be more efficient but applicable only in this case
             # if np.random.random() < (1 - self.epsilon):
@@ -168,7 +194,6 @@ class actionValue(object):
                 new_action.append(np.random.randint(0,self._action_space))
         return new_action
     def _get_action_not_hiveUpdate(self,s):
-        #ATTENTION never tested
         new_action = []
         for k in range(self._nAgents):
             sind,_a = self._get_index(s[k])
@@ -192,7 +217,8 @@ class actionValue(object):
         self.lr = self.scheduled_lr[self.n_episodes]
         self.epsilon = self.scheduled_epsilon[self.n_episodes]
         self.n_episodes+=1
-        #UPDATE OBSERVABLES
+
+        #UPDATE OBSERVABLES (costly)
         self._value.append(self.get_value())
         self._av_value.append(self.get_av_value())
         self._convergence.append(self._get_diff())
@@ -221,31 +247,55 @@ class actionValue(object):
             return make_binary(new_action) #need to have an instruction to be given to the tentacle (which sucker hangs)
         
     def _get_diff_hive(self):
-        diff = np.amax(np.abs(self._Q -self._oldQ))
-        self._oldQ = self._Q.copy()
-        return diff
+        diff =[]
+        for k in self._Q:
+            diff.append(np.abs(self._Q[k] -self._oldQ[k]))
+        self._oldQ = copy.deepcopy(self._Q)
+        #old implementation with Q not a dictionary
+        # diff = np.amax(np.abs(self._Q -self._oldQ))
+        # self._oldQ = self._Q.copy()
+        return np.amax(np.array(diff))
     
     def _get_diff_noHive(self):
-        diff = []
-        for k in range(self._nAgents):
-            diff.append(np.amax(np.abs(self._Q[k] -self._oldQ[k])))
-            self._oldQ[k] = self._Q[k].copy()
+        diff =[]
+        for i in range(self._nAgents):
+            d = []
+            for k in self._Q[i]:
+                d.append(np.abs(self._Q[i][k] -self._oldQ[i][k]))
+            
+            diff.append(np.amax(np.array(d)))
+            self._oldQ[i] = copy.deepcopy(self._Q[i])
         return diff
 
     def _get_value_hive(self):
-        return np.vstack((np.amax(self._Q,axis=1),np.argmax(self._Q,axis=1))).T
+        value = {}
+        for k in self._Q:
+            value[stateName[k]]=(np.amax(self._Q[k]),np.argmax(self._Q[k]))
+        return value
+        #return np.vstack((np.amax(self._Q,axis=1),np.argmax(self._Q,axis=1))).T
+
     def _get_av_value_hive(self):
-        return np.mean(self._get_value_hive()[:,0])
+        value = self._get_value_hive()
+        return np.mean([value[k][0] for k in value])
+        # return np.mean(self._get_value_hive()[:,0])
     
     def _get_value_noHive(self):
         v = []
-        for k in range(self._nAgents):
-            v.append(np.vstack((np.amax(self._Q[k],axis=1),np.argmax(self._Q[k],axis=1))).T)
+        # for k in range(self._nAgents):
+        #     v.append(np.vstack((np.amax(self._Q[k],axis=1),np.argmax(self._Q[k],axis=1))).T)
+        for i in range(self._nAgents):
+            value = {}
+            Q = self._Q[i]
+            for k in Q:
+                value[stateName[k]]=(np.amax(Q[k]),np.argmax(Q[k]))
+            v.append(value)
         return v
     def _get_av_value_noHive(self):
         avV =[]
+        value = self._get_value_noHive()
         for k in range(self._nAgents):
-            avV.append(np.mean(np.amax(self._Q[k],axis=1)))
+            vv = value[k]
+            avV.append(np.mean([vv[i][0] for i in vv]))
         return avV
     
     def get_conv(self):
@@ -258,29 +308,31 @@ class actionValue(object):
         self._fig_value.set_title(label='Value ('+str(self._state_space) + ' states)')
         sub_sampling = 10
         # last = int(self._greedySteps/sub_sampling)
-        values = np.array(self._value)[0:len(self._value):sub_sampling]
+        values = self._value[0:len(self._value):sub_sampling]
         episodes = [e for e in range(0,self.n_episodes+1,sub_sampling)]
         color =['blue','red']
-        actionState=[' not anchoring',' anchoring']
-        stateName = ['->|<- ','->|-> ','->|tip ','<-|<- ','<-|-> ','<-|tip ','base|<- ','base|-> ']
-        for i in range(self._state_space):
-            c = color[int(np.array(self._value)[-1,i,1])]#]*int((self.n_episodes+1)/sub_sampling)
-            a = actionState[int(np.array(self._value)[-1,i,1])]
+        for i in stateName:
+            a = actionState[int(self._value[-1][i][1])]
             # print(values[-1,i,:])
             #print(int(values[values.shape[0]-1,i,1]))
-            self._fig_value.plot(episodes,values[:,i,0],'-o',label=stateName[i]+a)
+            polVal = [v[i][0] for v in values]
+            # c = color[int(polVal[-1][1])]#]*int((self.n_episodes+1)/sub_sampling)
+            self._fig_value.plot(episodes,polVal,'-o',label=i+a)
+
             # self._fig_value.plot(episodes[-last:],values[-last:,i,0],color=c)
         self._fig_value.legend()
         
-        for i in range(self._state_space):
+        for i in stateName:
             plt.figure(figsize=(10, 6))
             self._fig_value_action_all = plt.subplot(xlabel='episodes', ylabel='action')
-            self._fig_value_action_all.set_title(label='policy jumps for ' + stateName[i])
-            self._fig_value_action_all.plot(episodes,values[:,i,1],'-x')
+            self._fig_value_action_all.set_title(label='policy jumps for ' + i)
+            polAction = [v[i][1] for v in values]
+            self._fig_value_action_all.plot(episodes,polAction,'-x')
             # self._fig_value.plot(episodes[-last:],values[-last:,i,0],color=c)
         
     def _plot_value_noHive(self):
-        n = input("sucker (agent) number")
+        n = int(input("sucker (agent) number"))
+        stateName = self._value[0][n].keys()
         plt.figure(figsize=(10, 6))
         self._fig_value = plt.subplot(xlabel='episodes', ylabel='value')
         self._fig_value .set_title(label='Value ('+str(self._state_space) + ' states)' + 'sucker '+str(n))
@@ -288,21 +340,24 @@ class actionValue(object):
         episodes = [e for e in range(0,self.n_episodes+1,sub_sampling)]
         # last = int(self._greedySteps/sub_sampling)
 
-        values = np.array(self._value[n])[0:len(self._value):sub_sampling]
+        values =  [v[n] for v in self._value[0:len(self._value):sub_sampling]]#np.array(self._value)[0:len(self._value):sub_sampling,n]
         
-        actionState=[' not anchoring',' anchoring']
-        stateName = ['->|<- ','->|-> ','->|tip ','<-|<- ','<-|-> ','<-|tip ','base|<- ','base|-> ']
-        for i in range(self._state_space):
-            a = actionState[int(np.array(self._value[n])[-1,i,1])]
-            self._fig_value.plot(episodes,values[:,i,0],'-o',label=stateName[i]+a)
+        
+
+        for i in stateName:
+        #     a = actionState[int(np.array(self._value)[:,n][-1,i,1])]
+            a = actionState[int(values[-1][i][1])]
+            polVal = [v[i][0] for v in values]
+            self._fig_value.plot(episodes,polVal,'-o',label=i+a)
             # self._fig_value.plot(episodes[-last:],values[-last:,i,0],color=c)
         self._fig_value.legend()     
 
-        for i in range(self._state_space):
+        for i in stateName:
             plt.figure(figsize=(10, 6))
             self._fig_value_action_all = plt.subplot(xlabel='episodes', ylabel='action')
-            self._fig_value_action_all.set_title(label='Sucker '+ str(n) +'. Policy jumps for ' + stateName[i])
-            self._fig_value_action_all.plot(episodes,values[:,i,1],'-x')
+            self._fig_value_action_all.set_title(label='Sucker '+ str(n) +'. Policy jumps for ' + i)
+            polAction = [v[i][1] for v in values]
+            self._fig_value_action_all.plot(episodes,polAction,'-x')
 
 
     
@@ -314,12 +369,13 @@ class actionValue(object):
         self._fig_av_value.plot(episodes,self._av_value)
 
     def _plot_av_value_noHive(self):
-        n = input("sucker (agent) number")
+        n = int(input("sucker (agent) number"))
         plt.figure()
         self._fig_av_value = plt.subplot(xlabel='episode', ylabel='average_value')
         self._fig_av_value.set_title(label='Average value sucker '+str(n))
         episodes = [e for e in range(self.n_episodes+1)]
-        self._fig_av_value.plot(episodes,self._av_value[n])
+        avValue = np.array(self._av_value)[:,n]
+        self._fig_av_value.plot(episodes,avValue)
 
     
     def _plot_convergence_hive(self):
@@ -330,11 +386,12 @@ class actionValue(object):
         self._fig_convergence.plot(episodes,self._convergence)
     
     def _plot_convergence_noHive(self):
-        n = input("sucker (agent) number")
+        n = int(input("sucker (agent) number"))
         plt.figure()
         self._fig_convergence = plt.subplot(xlabel='episode', ylabel='convergence')
         self._fig_convergence.set_title(label='Global convergence Q function for sucker  '+str(n))
         episodes = [e for e in range(self.n_episodes)]
-        self._fig_convergence.plot(episodes,self._convergence)
+        convergence = np.array(self._convergence)[:,n]
+        self._fig_convergence.plot(episodes,convergence)
 
    
