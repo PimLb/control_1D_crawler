26c43a166b6da2fcb3a769d5cd5532c935e32abd
Luca Gagliardi <lucaj.1991@gmail.com>
2023-10-16 11:45:32 +0200

    Container of commont libraries and parameters

---
+++ globals.py
@@ -0,0 +1,15 @@
+red = (220, 20, 60)
+blue = (30, 144, 255)
+green = (0,128,0)
+brown = (128,0,0)
+dark_violet = (199,21,133)
+violet = (219,112,147)
+
+#some optimization..
+# from numba import jit
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import math
+import pygame
+import copy 
